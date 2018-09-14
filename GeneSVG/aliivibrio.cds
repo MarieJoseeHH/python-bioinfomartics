02264442b503df8d8cf4712bfe755b78149754f9
Samonios <marie-josee.h.halsor@uit.no>
2018-09-14 13:52:38 +0200

    cds2svg.py update

---
+++ GeneSVG/aliivibrio.cds
@@ -0,0 +1,7 @@
+Aliivibrio salmonicida LFI1238
+257294;258475;hemY;reverse
+258472;259686;hemX;reverse
+259699;260445;hemD;reverse
+260447;261382;hemC;reverse
+261640;264150;cya
+264252;266456;uvrD
--- GeneSVG/cds2svg.py
+++ GeneSVG/cds2svg.py
@@ -16,21 +16,21 @@ Aliivibrio_R8-70_concat
 """
 import sys
 
-def set_width(cds_table):
+def set_width(cds_table,organsim):
     """Extracts start and stop bases from input file.
 
     Calculates the length of sequence to be displayed, thus allowing horizontal size"""
 
     start=cds_table[1].split(';')[0]
     stop=cds_table[-1].split(';')[1]
-    svg_width=abs(int(stop)-int(start))//15
+    svg_width=abs(int(stop)-int(start))//4
     return(start,stop,svg_width)
 
 def set_coordinates(cds_table,organism,start):
     """Extracts required coordinates for svg paths"""
 
     arrow_data=[]
-    x0=len(organism)*19
+    x0=len(organism)*20
     xshift=int(start)//6-x0
     for i,p in enumerate(cds_table):
         cds_start=p.split(';')[0]
@@ -40,13 +40,13 @@ def set_coordinates(cds_table,organism,start):
         x1=int(cds_start)//6-xshift
         #check for strand oritentation
         if len(p.split(';'))==4:#reverse
-            y1=100
+            y1=200
             x2,y2=25,-50
             x3,y3=path_length-25,0
             x4,y4=0,100
             x5,y5=-(path_length-25),0
         else:
-            y1=50
+            y1=150
             x2,y2=path_length-25,0
             x3,y3=25,50
             x4,y4=-25,50
@@ -64,7 +64,7 @@ def draw_arrow (arrow_data):
     path_style='   fill="none" stroke="black" stroke-width="3"/>\n'
     for i,p in enumerate(arrow_data):
         arrow_path='\n<path d="M'+str(p[1])+','+str(p[2])+' l'+str(p[3])+','+str(p[4])+' l'+str(p[5])+','+str(p[6])+' l'+str(p[7])+','+str(p[8])+' l'+str(p[9])+','+str(p[10])+'z" '
-        arrow_text='<text x="'+str(p[1]+25)+' " y="40" transform="rotate(-45 '+str(p[1]+25)+' 840)" font-size="40"> '+p[0]+'</text>\n'
+        arrow_text='<text x="'+str(p[1]+25)+' " y="140" transform="rotate(-45 '+str(p[1]+25)+' 140)" font-size="40"> '+p[0]+'</text>\n'
         svg_arrow=svg_arrow+arrow_path+path_style+arrow_text
     return(svg_arrow)
 
@@ -74,7 +74,7 @@ def code_svg (svg_width,organism,svg_arrow):
 
     svg_code="""<?xml version="1.0" standalone="no"?>
 <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" 
-    "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n"""+'<svg width="'+str(svg_width)+'" height="200"\n'+'    xmlns="http://www.w3.org/2000/svg" version="1.1">\n'+'<title>CDS organisation-'+organism+'('+start+'..'+stop+')</title>\n'+'\n<text x="10" y="110" font-size="40" font-style="italic">'+organism+'</text>\n'+svg_arrow+'</svg>'
+    "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">\n"""+'<svg width="'+str(svg_width)+'" height="400"\n'+'    xmlns="http://www.w3.org/2000/svg" version="1.1">\n'+'<title>CDS organisation-'+organism+'('+start+'..'+stop+')</title>\n'+'\n<text x="10" y="210" font-size="40" font-style="italic">'+organism+'</text>\n'+svg_arrow+'</svg>'
     return(svg_code)
 
 inputfile=sys.argv[1]
@@ -82,8 +82,8 @@ ifile=open(inputfile,'r')
 cds_table=ifile.readlines()
 ifile.close()
 
-start,stop,svg_width=set_width(cds_table)
 organism=cds_table[0].replace('\n','')
+start,stop,svg_width=set_width(cds_table,organism)
 del cds_table[0]
 
 arrow_data=set_coordinates(cds_table,organism,start)
